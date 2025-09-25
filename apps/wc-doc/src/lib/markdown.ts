c9e9398e29d9b72102b1f31ae3c80a140f0d6718
criss8X <carralerocristianx@gmail.com>
2025-09-25 13:26:01 -0400

    fix: Resolver package.json problem

--- apps/wc-doc/src/lib/markdown.ts
+++ apps/wc-doc/src/lib/markdown.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Regex for bold sentences and line breaks.
 const MD_REGEX_CM = /\*\*(.*?)\*\*|\n/g;
 
@@ -17,12 +16,5 @@ export function parseMinimalMdToHtml(
 		}
 
 		return `<${bold} class="highlight">${match.slice(2, -2)}</${bold}>`;
-=======
-const HIGHLIGHT_REGEX = /\*{2}(.*?)\*{2}/gms;
-
-export function parseHighlightsFromParagraph(paragraph: string): string {
-	return paragraph.replace(HIGHLIGHT_REGEX, (match) => {
-		return `<em class="bg-blue-900/50 px-1 not-italic">${match.slice(2, -2)}</em>`;
->>>>>>> 52edd2f (feat: Implement getting started basic)
 	});
 }
