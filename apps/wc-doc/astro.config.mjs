b7923b12f714ee35a60e80b2b6d5fb573b3392b5
criss8X <carralerocristianx@gmail.com>
2025-10-02 15:20:24 -0400

    chore: Implement before method of translations

--- apps/wc-doc/astro.config.mjs
+++ apps/wc-doc/astro.config.mjs
@@ -11,6 +11,14 @@ export default defineConfig({
 			include: ["class-variance-authority"],
 		},
 	},
+	i18n: {
+		defaultLocale: "en",
+		locales: ["en", "es"],
+		routing: {
+			prefixDefaultLocale: false,
+			redirectToDefaultLocale: true,
+		},
+	},
 
 	outDir: "../../docs",
 	base: "wallet-connect",
