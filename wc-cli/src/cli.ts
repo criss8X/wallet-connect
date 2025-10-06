115863c760bd74847f918a4a996ec443b58335c5
criss8X <carralerocristianx@gmail.com>
2025-10-06 19:11:03 -0400

    refactor: Clean before code of cli

--- wc-cli/src/cli.ts
+++ wc-cli/src/cli.ts
@@ -1,26 +1,2 @@
 #!/usr/bin/env node
-
-import { setupWalletConnect } from "./wallet-connect/index.js";
-
-const [, , cmd, name, ...args] = process.argv;
-
-switch (cmd) {
-	case "add": {
-		add(name, args);
-		break;
-	}
-}
-
-async function add(nameOfComponent: string, args: string[]) {
-	const rootDir = process.cwd();
-
-	switch (nameOfComponent) {
-		case "wallet-connect": {
-			return await setupWalletConnect(rootDir, args);
-		}
-
-		default: {
-			throw new Error(`The component ${nameOfComponent} was not found.`);
-		}
-	}
-}
+console.log("Hello World");
